d8584fb76ff42c2f69159de591203f3fa704f429
Luciana Abecasis <luciana.abecasis@phinxlab.com>
2020-04-08 10:45:49 -0300

    search done

--- src/containers/CRUD/CRUD.tsx
+++ src/containers/CRUD/CRUD.tsx
@@ -5,30 +5,53 @@ import './CRUD.scss';
 
 
 const CRUD = ({searchTodo})=>{
-    const [filter, setFilter] =useState<undefined|string>(undefined);
     const [content, setContent]=useState<string>();
     const [searchBy, setSearchBy]=useState<string>();
+    const [showFilters, setShowFilters]=useState<boolean>(true);
+    const [showInput, setShowInput]=useState<boolean>(true);
+    const [filter, setFilter] =useState<undefined|string>(undefined);
 
      const submitSearch = ()=>{
-        if (content === undefined) {
+        if (filter === undefined) {
             searchTodo(searchBy,content);    
-        }else {
+        }else if(searchBy === "all") {
+            searchTodo("completed",filter)
+        }
+        else {
             searchTodo(searchBy,content,filter);
         }
      }
-
+     const showNotShow= (selectValue) => {
+        switch (selectValue) {
+            case "id":
+                setShowFilters(false);
+                setShowInput(true);
+                break
+            case "all":
+                setShowFilters(true);
+                setShowInput(false);
+                break
+            case "userId":
+            default:
+                setShowFilters(true);
+                setShowInput(true);
+        }
+     }
     return(
         <div className="actions">
-            <select name="param" onChange={e=>{setSearchBy(e.target.value)}}>
+            <select name="param" onChange={e=>{ showNotShow(e.target.value)
+                                                setSearchBy(e.target.value)}}>
+                <option>Search by...</option>
                 <option value="id"> ID </option>
                 <option value="userId"> User ID </option>
+                <option value="all"> All </option>
             </select>
-            <input type="text" onChange={e=>{setContent(e.target.value)}}/>
-            <div id="todo-completed-filter">
+            {showInput? <input type="text" onChange={e=>{setContent(e.target.value)}}/>:null}
+           {showFilters? <div id="todo-completed-filter">
                 <input name="filter" type="radio" value="all" onChange={e=>{setFilter(undefined)}}/> Todas
                 <input name="filter" type="radio" value="true" onChange={e=>{setFilter(e.target.value)}}/> Completadas
                 <input name="filter" type="radio" value="false" onChange={e=>{setFilter(e.target.value)}}/> Pendientes
-            </div>
+            </div> : null}
             <button type="button" onClick={submitSearch}>Ok!</button>
             
         </div>
